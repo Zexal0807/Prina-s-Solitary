896c156991e1fe6e96bac8109a649a4f1dd40646
Zexal0807 <gallinar00@gmail.com>
2022-11-07 13:26:20 +0100

    Add all cards

--- Deck.js
+++ Deck.js
@@ -8,6 +8,48 @@ module.exports = class Deck {
 
     constructor() {
         this.cards.push(new Card(Seme.HEART, Value.ONE));
+        this.cards.push(new Card(Seme.HEART, Value.TWO));
+        this.cards.push(new Card(Seme.HEART, Value.THREE));
+        this.cards.push(new Card(Seme.HEART, Value.FOUR));
+        this.cards.push(new Card(Seme.HEART, Value.FIVE));
+        this.cards.push(new Card(Seme.HEART, Value.SIX));
+        this.cards.push(new Card(Seme.HEART, Value.SEVEN));
+        this.cards.push(new Card(Seme.HEART, Value.JACK));
+        this.cards.push(new Card(Seme.HEART, Value.QUEEN));
+        this.cards.push(new Card(Seme.HEART, Value.KING));
+
+        this.cards.push(new Card(Seme.DIAMOND, Value.ONE));
+        this.cards.push(new Card(Seme.DIAMOND, Value.TWO));
+        this.cards.push(new Card(Seme.DIAMOND, Value.THREE));
+        this.cards.push(new Card(Seme.DIAMOND, Value.FOUR));
+        this.cards.push(new Card(Seme.DIAMOND, Value.FIVE));
+        this.cards.push(new Card(Seme.DIAMOND, Value.SIX));
+        this.cards.push(new Card(Seme.DIAMOND, Value.SEVEN));
+        this.cards.push(new Card(Seme.DIAMOND, Value.JACK));
+        this.cards.push(new Card(Seme.DIAMOND, Value.QUEEN));
+        this.cards.push(new Card(Seme.DIAMOND, Value.KING));
+
+        this.cards.push(new Card(Seme.FLOWER, Value.ONE));
+        this.cards.push(new Card(Seme.FLOWER, Value.TWO));
+        this.cards.push(new Card(Seme.FLOWER, Value.THREE));
+        this.cards.push(new Card(Seme.FLOWER, Value.FOUR));
+        this.cards.push(new Card(Seme.FLOWER, Value.FIVE));
+        this.cards.push(new Card(Seme.FLOWER, Value.SIX));
+        this.cards.push(new Card(Seme.FLOWER, Value.SEVEN));
+        this.cards.push(new Card(Seme.FLOWER, Value.JACK));
+        this.cards.push(new Card(Seme.FLOWER, Value.QUEEN));
+        this.cards.push(new Card(Seme.FLOWER, Value.KING));
+
+        this.cards.push(new Card(Seme.CLUB, Value.ONE));
+        this.cards.push(new Card(Seme.CLUB, Value.TWO));
+        this.cards.push(new Card(Seme.CLUB, Value.THREE));
+        this.cards.push(new Card(Seme.CLUB, Value.FOUR));
+        this.cards.push(new Card(Seme.CLUB, Value.FIVE));
+        this.cards.push(new Card(Seme.CLUB, Value.SIX));
+        this.cards.push(new Card(Seme.CLUB, Value.SEVEN));
+        this.cards.push(new Card(Seme.CLUB, Value.JACK));
+        this.cards.push(new Card(Seme.CLUB, Value.QUEEN));
+        this.cards.push(new Card(Seme.CLUB, Value.KING));
 
 
     }
