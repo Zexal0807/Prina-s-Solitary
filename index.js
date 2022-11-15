68560b7a7def7a90d97ef6442b0559786c04d835
Zexal0807 <gallinar00@gmail.com>
2022-11-15 08:48:16 +0100

    Add log

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const Deck = require("./Deck");
 
 let s = 40;
 let d;
+let tent = 0;
+console.log(new Date())
 do {
     d = new Deck();
     d.shaffle();
     s = d.perfectCheck();
+    tent++;
+    if (tent % 1000000 == 0) {
+        console.log("Tentativi: " + tent / 1000000 + ".000.000");
+    }
 } while (s != 1);
-console.log("Sequenza lunga " + s);
+console.log("Sequenza lunga " + s + " in " + tent + " tentativi");
+console.log(new Date())
 d.print();
