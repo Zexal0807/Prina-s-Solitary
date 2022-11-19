f0f89e5296f0d4e6594db4a22d18d56d9a4725d5
Zexal0807 <gallinar00@gmail.com>
2022-11-19 23:57:29 +0100

    Add more statistics

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const Deck = require("./Deck");
 
-const MAX_TENT = 1000 * 1000 * 1000;
+const MAX_TENT = 10 * 1000 * 1000;
+
+let result = [];
+
 let correct = 0;
 let perfect = 0;
+let perfectInCorrect = 0;
 let n_perfect = 0;
 
 let d;
@@ -12,13 +16,20 @@ for (let i = 0; i < MAX_TENT; i++) {
 	d = new Deck();
 	d.shaffle();
 
-	if (d.isCorrect()) {
+	let ss = d._check([...d.cards]);
+	if (result[ss] == undefined) {
+		result[ss] = 0;
+	}
+	result[ss]++;
+
+	if (ss == 2) {
 		correct++;
 		if (d.isPerfect()) {
-			perfect++
+			perfect++;
+			perfectInCorrect++;
 		}
 	}
-	if (d.isNPerfect()) {
+	if (ss == 40) {
 		d.print();
 		n_perfect++;
 	}
@@ -29,4 +40,7 @@ console.log("TOT", (new Date() - s) / 1000, "s");
 
 console.log(`Correct: ${correct}/${MAX_TENT} (${correct/MAX_TENT*100}%)`);
 console.log(`Perfect: ${perfect}/${MAX_TENT} (${perfect/MAX_TENT*100}%)`);
+console.log(`Perfect IN Correct: ${perfectInCorrect}/${correct} (${perfectInCorrect/correct*100}%)`);
 console.log(`N-Perfect: ${n_perfect}/${MAX_TENT} (${n_perfect/MAX_TENT*100}%)`);
+
+console.log(result.map(x => { return { tent: x, perc: x / MAX_TENT * 100 } }));
